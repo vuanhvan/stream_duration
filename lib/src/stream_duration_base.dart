64dc746106443edb2c70394195e95265c1dedb57
vuanhvan <vuanhvan@hotmail.com>
2021-10-09 15:10:54 +0700

    non null safe

--- lib/src/stream_duration_base.dart
+++ lib/src/stream_duration_base.dart
@@ -1,17 +1,15 @@
 import 'dart:async';
 
 class StreamDuration {
-  late final StreamController<Duration> _streamController =
-      StreamController<Duration>();
+  final StreamController<Duration> _streamController = StreamController<Duration>();
   Stream<Duration> get durationLeft => _streamController.stream;
-  StreamSubscription<Duration>? _streamSubscription;
+  StreamSubscription<Duration> _streamSubscription;
 
   StreamDuration(Duration duration, Function onDone) {
     try {
       var _durationLeft = duration;
 
-      _streamSubscription =
-          Stream<Duration>.periodic(Duration(seconds: 1), (_) {
+      _streamSubscription = Stream<Duration>.periodic(Duration(seconds: 1), (_) {
         return _durationLeft -= Duration(seconds: 1);
       }).listen((event) {
         if (!_streamController.isClosed) {
